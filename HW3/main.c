7764d0ecb0d0acbe75aa1b43a4aebcce870b447f
Jongseo Kang <kjs010602@naver.com>
2024-04-24 00:10:46 +0900

    main.c

--- HW3/main.c
+++ HW3/main.c
@@ -3,7 +3,10 @@
 #include <string.h>
 #include <openssl/aes.h>
 #include <openssl/sha.h>
+#include <openssl/evp.h>
+#include <openssl/hmac.h>
 
+#define HMAC_DIGEST_LENGTH 32 // SHA-256 output length in bytes
 #define AES_BLOCK_SIZE 16
 #define BUFFER_SIZE 4096
 #define ENC 1130
@@ -12,18 +15,23 @@
 int mode;
 char *key, *in, *out, *tag;
 
+int isArg(char *arg);
 int inputCheck(int argc, char **argv);
 void sha256HashFile(const char *filename, unsigned char *hash);
 int removePadding(unsigned char *buffer, int length);
 void decryptFile(const char *inputFilename, const char *outputFilename, const unsigned char *key);
 void addPadding(unsigned char *inputBuffer, size_t bytesRead, size_t blockSize);
 void encryptFile(const char *inputFilename, const char *outputFilename, const unsigned char *key);
+void hmac_sha256(const unsigned char *key, const unsigned char *message, unsigned int message_len, unsigned char *digest);
+int hmac_sha256_verify(const unsigned char *key, const unsigned char *message, unsigned int message_len, const unsigned char *expected_digest);
+unsigned char *fileReader(const char *fileName, unsigned int *len);
+void fileRecorder(const char *fileName, const unsigned char *message, unsigned int len);
 
 int main(int argc, char **argv)
 {
     if (inputCheck(argc, argv))
     {
-        printf("ERROR\n");
+        puts("ERROR");
 
         free(key);
         free(in);
@@ -32,104 +40,139 @@ int main(int argc, char **argv)
         return 2;
     }
 
-    char hashedKey[SHA256_DIGEST_LENGTH];
+    unsigned char hashedKey[SHA256_DIGEST_LENGTH], digest[HMAC_DIGEST_LENGTH];
+    unsigned char *tagMessage, *encMessage;
+    unsigned int tagMessageLen = 0, encMessageLen = 0;
 
-    if(mode == ENC){
+    if (mode == ENC)
+    {
         sha256HashFile(key, hashedKey);
         encryptFile(in, out, hashedKey);
+        encMessage = fileReader(out, &encMessageLen);
+        hmac_sha256(hashedKey, encMessage, encMessageLen, digest);
+        fileRecorder(tag, digest, HMAC_DIGEST_LENGTH);
+
+        free(encMessage);
+        free(key);
+        free(in);
+        free(out);
+        free(tag);
+        return 0;
     }
-    else if(mode == DEC){
+    else if (mode == DEC)
+    {
         sha256HashFile(key, hashedKey);
-        decryptFile(in, out, hashedKey);
+        encMessage = fileReader(in, &encMessageLen);
+        tagMessage = fileReader(tag, &tagMessageLen);
+
+        if(tagMessageLen == HMAC_DIGEST_LENGTH){
+            memcpy(digest, tagMessage, HMAC_DIGEST_LENGTH);
+        }
+        else{
+            puts("VERIFICATION FAILURE");
+
+            free(tagMessage);
+            free(encMessage);
+            free(key);
+            free(in);
+            free(out);
+            free(tag);
+            return 1;
+        }
+
+
+        if (hmac_sha256_verify(hashedKey, encMessage, encMessageLen, digest))
+        {
+            decryptFile(in, out, hashedKey);
+
+            free(tagMessage);
+            free(encMessage);
+            free(key);
+            free(in);
+            free(out);
+            free(tag);
+            return 0;
+        }
+        else
+        {
+            puts("VERIFICATION FAILURE");
+
+            free(tagMessage);
+            free(encMessage);
+            free(key);
+            free(in);
+            free(out);
+            free(tag);
+            return 1;
+        }
     }
 
+    return 0;
+}
 
+int isArg(char *arg){
+    if(strcmp(arg, "-key") == 0) return 1;
+    if(strcmp(arg, "-tag") == 0) return 1;
+    if(strcmp(arg, "-in") == 0) return 1;
+    if(strcmp(arg, "-out") == 0) return 1;
 
-    free(key);
-    free(in);
-    free(out);
-    free(tag);
     return 0;
 }
 
 int inputCheck(int argc, char **argv)
 {
-    if (argc & 1)
+    if (argc != 10)
         return 1;
 
-    // for safe realloc()
-    char *buffer;
-
-    int keyCheck, inCheck, outCheck, tagCheck;
-    keyCheck = inCheck = outCheck = tagCheck = 0;
-
-    key = malloc(strlen("shared.key"));
-    strcpy(key, "shared.key");
-    tag = malloc(strlen("encrypted.tag"));
-    strcpy(tag, "encrypted.tag");
+    int keyCheck, inCheck, outCheck, tagCheck, argnums;
+    keyCheck = inCheck = outCheck = tagCheck = argnums = 0;
 
     if (strcmp(argv[1], "enc") == 0)
-    { // encode mode
         mode = ENC;
-        in = malloc(strlen("original.txt"));
-        strcpy(in, "original.txt");
-        out = malloc(strlen("encrypted.txt"));
-        strcpy(out, "encrypted.txt");
-    }
-    else if (strcmp(argv[1], "dec") == 0)
-    { // decode mode
+    else if(strcmp(argv[1], "dec") == 0)
         mode = DEC;
-        in = malloc(strlen("encrypted.txt"));
-        strcpy(in, "encrypted.txt");
-        out = malloc(strlen("decrypted.txt"));
-        strcpy(out, "decrypted.txt");
-    }
     else
         return 1;
-
 
     for (int i = 2; i < argc; i += 2)
     {
         if (!keyCheck && strcmp(argv[i], "-key") == 0)
         {
+            if (isArg(argv[i + 1])) return 1;
             keyCheck++;
-            buffer = key;
-            if (NULL == (key = realloc(key, strlen(argv[i + 1]))))
-                free(buffer);
+            argnums++;
+            key = malloc(strlen(argv[i + 1]));
             strcpy(key, argv[i + 1]);
         }
         else if (!inCheck && strcmp(argv[i], "-in") == 0)
         {
+            if (isArg(argv[i + 1])) return 1;
             inCheck++;
-            buffer = in;
-            if (NULL == (in = realloc(in, strlen(argv[i + 1]))))
-                free(buffer);
-
+            argnums++;
+            in = malloc(strlen(argv[i + 1]));
             strcpy(in, argv[i + 1]);
         }
         else if (!outCheck && strcmp(argv[i], "-out") == 0)
         {
+            if (isArg(argv[i + 1])) return 1;
             outCheck++;
-            buffer = out;
-            if (NULL == (out = realloc(out, strlen(argv[i + 1]))))
-                free(buffer);
-
+            argnums++;
+            out = malloc(strlen(argv[i + 1]));
             strcpy(out, argv[i + 1]);
         }
         else if (!tagCheck && strcmp(argv[i], "-tag") == 0)
         {
+            if (isArg(argv[i + 1])) return 1;
             tagCheck++;
-            buffer = tag;
-            if (NULL == (tag = realloc(tag, strlen(argv[i + 1]))))
-                free(buffer);
-
+            argnums++;
+            tag = malloc(strlen(argv[i + 1]));
             strcpy(tag, argv[i + 1]);
         }
         else
             return 1;
     }
 
-    return 0;
+    return argnums < 4;
 }
 
 void sha256HashFile(const char *filename, unsigned char *hash)
@@ -157,24 +200,21 @@ void sha256HashFile(const char *filename, unsigned char *hash)
     fclose(file);
 }
 
-
-int removePadding(unsigned char *buffer, int length) {
+int removePadding(unsigned char *buffer, int length)
+{
     unsigned char paddingValue = buffer[length - 1];
     return length - paddingValue;
 }
 
-void decryptFile(const char *inputFilename, const char *outputFilename, const unsigned char *key) {
+void decryptFile(const char *inputFilename, const char *outputFilename, const unsigned char *key)
+{
     FILE *inputFile = fopen(inputFilename, "rb");
     FILE *outputFile = fopen(outputFilename, "wb");
 
-    if (!inputFile || !outputFile) {
-        perror("Error opening files");
-        return;
-    }
-
     // Initialize AES decryption context
     AES_KEY aesKey;
-    if (AES_set_decrypt_key(key, 256, &aesKey) != 0) {
+    if (AES_set_decrypt_key(key, 256, &aesKey) != 0)
+    {
         perror("AES_set_decrypt_key failed");
         fclose(inputFile);
         fclose(outputFile);
@@ -186,20 +226,22 @@ void decryptFile(const char *inputFilename, const char *outputFilename, const un
     unsigned char outputBuffer[AES_BLOCK_SIZE];
     size_t bytesRead, nextBytes;
 
-
     bytesRead = fread(inputBuffer, 1, AES_BLOCK_SIZE, inputFile);
-    while (bytesRead > 0) {
+    while (bytesRead > 0)
+    {
         // Decrypt each block
         nextBytes = fread(nextBuffer, 1, AES_BLOCK_SIZE, inputFile);
         AES_decrypt(inputBuffer, outputBuffer, &aesKey);
-        if(nextBytes == 0) break;
+        if (nextBytes == 0)
+            break;
         fwrite(outputBuffer, 1, AES_BLOCK_SIZE, outputFile);
         strcpy(inputBuffer, nextBuffer);
         bytesRead = nextBytes;
     }
 
     bytesRead = removePadding(outputBuffer, AES_BLOCK_SIZE);
-    if(bytesRead){
+    if (bytesRead)
+    {
         fwrite(outputBuffer, 1, bytesRead, outputFile);
     }
 
@@ -207,25 +249,24 @@ void decryptFile(const char *inputFilename, const char *outputFilename, const un
     fclose(outputFile);
 }
 
-void addPadding(unsigned char *inputBuffer, size_t bytesRead, size_t blockSize) {
+void addPadding(unsigned char *inputBuffer, size_t bytesRead, size_t blockSize)
+{
     size_t paddingLength = blockSize - (bytesRead % blockSize);
-    for (size_t i = 0; i < paddingLength; i++) {
+    for (size_t i = 0; i < paddingLength; i++)
+    {
         inputBuffer[blockSize - i - 1] = (unsigned char)paddingLength;
     }
 }
 
-void encryptFile(const char *inputFilename, const char *outputFilename, const unsigned char *key) {
+void encryptFile(const char *inputFilename, const char *outputFilename, const unsigned char *key)
+{
     FILE *inputFile = fopen(inputFilename, "rb");
     FILE *outputFile = fopen(outputFilename, "wb");
 
-    if (!inputFile || !outputFile) {
-        perror("Error opening files");
-        return;
-    }
-
     // Initialize AES encryption context
     AES_KEY aesKey;
-    if (AES_set_encrypt_key(key, 256, &aesKey) != 0) {
+    if (AES_set_encrypt_key(key, 256, &aesKey) != 0)
+    {
         perror("AES_set_encrypt_key failed");
         fclose(inputFile);
         fclose(outputFile);
@@ -233,15 +274,17 @@ void encryptFile(const char *inputFilename, const char *outputFilename, const un
     }
 
     unsigned char inputBuffer[AES_BLOCK_SIZE]; // Twice the block size for padding
-    unsigned char nextBuffer[AES_BLOCK_SIZE]; // Twice the block size for padding
+    unsigned char nextBuffer[AES_BLOCK_SIZE];  // Twice the block size for padding
     unsigned char outputBuffer[AES_BLOCK_SIZE];
     size_t bytesRead, nextBytes;
 
     bytesRead = fread(inputBuffer, 1, AES_BLOCK_SIZE, inputFile);
-    while (bytesRead > 0) {
+    while (bytesRead > 0)
+    {
         // Encrypt each block
         nextBytes = fread(nextBuffer, 1, AES_BLOCK_SIZE, inputFile);
-        if(nextBytes == 0) break;
+        if (nextBytes == 0)
+            break;
         AES_encrypt(inputBuffer, outputBuffer, &aesKey);
         fwrite(outputBuffer, 1, AES_BLOCK_SIZE, outputFile);
         strcpy(inputBuffer, nextBuffer);
@@ -249,10 +292,62 @@ void encryptFile(const char *inputFilename, const char *outputFilename, const un
     }
 
     // Add PKCS#7 padding
+    if (bytesRead == AES_BLOCK_SIZE)
+    {
+        AES_encrypt(inputBuffer, outputBuffer, &aesKey);
+        fwrite(outputBuffer, 1, AES_BLOCK_SIZE, outputFile);
+    }
     addPadding(inputBuffer, bytesRead, AES_BLOCK_SIZE);
     AES_encrypt(inputBuffer, outputBuffer, &aesKey);
     fwrite(outputBuffer, 1, AES_BLOCK_SIZE, outputFile);
 
     fclose(inputFile);
     fclose(outputFile);
+}
+
+void hmac_sha256(const unsigned char *key, const unsigned char *message, unsigned int message_len, 
+                 unsigned char *digest)
+{
+    HMAC_CTX *ctx;
+    int key_len = SHA256_DIGEST_LENGTH;
+
+    ctx = HMAC_CTX_new();
+    HMAC_Init_ex(ctx, key, key_len, EVP_sha256(), NULL);
+    HMAC_Update(ctx, message, message_len);
+    HMAC_Final(ctx, digest, &key_len);
+    HMAC_CTX_free(ctx);
+}
+
+int hmac_sha256_verify(const unsigned char *key, const unsigned char *message, unsigned int message_len,
+                       const unsigned char *expected_digest)
+{
+    unsigned char digest[HMAC_DIGEST_LENGTH];
+    int key_len = SHA256_DIGEST_LENGTH;
+
+    hmac_sha256(key, message, message_len, digest);
+
+    // Compare computed digest with expected digest
+    return memcmp(digest, expected_digest, HMAC_DIGEST_LENGTH) == 0;
+}
+
+unsigned char *fileReader(const char *fileName, unsigned int *len)
+{
+    FILE *input = fopen(fileName, "rb");
+    unsigned char *message = malloc(0);
+    unsigned char buffer[10];
+    
+    while(fread(buffer, sizeof(unsigned char), 1, input) > 0){
+        message = realloc(message, sizeof(char) * ++(*len));
+        message[*len - 1] = buffer[0];
+    }
+
+    fclose(input);
+    return message;
+}
+
+void fileRecorder(const char *fileName, const unsigned char *message, unsigned int len)
+{
+    FILE *output = fopen(fileName, "wb");
+    fwrite(message, sizeof(char), len, output);
+    fclose(output);
 }
