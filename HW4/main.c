d6f4d05974c4a8a13404822644490630257e235a
Jongseo Kang <kjs010602@naver.com>
2024-05-22 08:52:21 +0900

    포기

--- HW4/main.c
+++ HW4/main.c
@@ -1,35 +1,44 @@
 #define _GNU_SOURCE
 #include <sys/wait.h>
-#include <sys/mount.h>
 #include <sys/types.h>
+#include <sys/prctl.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <unistd.h>
 #include <signal.h>
 #include <sched.h>
-#include <stdio.h>
-#include <string.h>
 #include <dlfcn.h>
-#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <seccomp.h>
+
 
 #define STACK_SIZE (1024 * 1024)
 
 
-char *filePath, *mntPath, *topKstring;
+char *filePath, *topKstring;
 unsigned int topK;
-void (*image_classifier)(const char *, int);
-
 
 char childStack[STACK_SIZE];
+
+void (*image_classifier)(const char *, int);
+void sandbox();
+void makeResult();
+void readTemp(const char *dataLine);
 int child(void *arg);
 
 int main()
 {
+    makeResult();
+    
     FILE *request = fopen("requests.txt", "r");
 
-    void *handle = dlopen("libdarknet_predict.so", RTLD_LAZY);
-    printf("%s\n", dlerror());
+    void *handle = dlopen("./libdarknet_predict.so", RTLD_LAZY);
     image_classifier = (void (*)(const char *, int))dlsym(handle, "image_classifier");
 
-    printf("%s\n", dlerror());
     char *dataLine = malloc(0);
     int buffer;
     unsigned int len = 0;
@@ -42,27 +51,24 @@ int main()
 
             filePath = malloc(sizeof(char) * len);
             topKstring = malloc(sizeof(char) * len);
-            mntPath = malloc(sizeof(char) * (len + 7));
-            int i, j, cnt;
+            int i, cnt;
 
-            memcpy(mntPath, "./data/", 7);
-            for (i = j = cnt = 0; i < len; i++)
+            memcpy(filePath, "./data/", 7);
+            for (i = cnt = 0; i < len; i++)
             {
                 if (dataLine[i] == ':')
                 {
                     cnt++;
                     if (cnt == 1)
-                        mntPath[i + 7] = '\0';
+                        filePath[i + 7] = '/';
                     if (cnt == 2)
                     {
-                        filePath[j] = '\0';
+                        filePath[i + 7] = '\0';
                         break;
                     }
                 }
-                else if(cnt < 1)
-                    mntPath[i + 7] = dataLine[i];
-                else if(cnt < 2)
-                    filePath[j++] = dataLine[i];
+                else
+                    filePath[i + 7]= dataLine[i];
             }
 
             for (cnt = 0, i++; i < len; i++, cnt++)
@@ -70,15 +76,14 @@ int main()
 
             topK = atoi(topKstring);
 
-            // int childPID = clone(child, (void*)(childStack + STACK_SIZE), CLONE_NEWNS | CLONE_NEWNET | SIGCHLD, NULL);
+            int childPID = clone(child, (void*)(childStack + STACK_SIZE), CLONE_NEWUSER | CLONE_NEWNS | CLONE_NEWNET | SIGCHLD, NULL);
 
-            // waitpid(childPID, NULL, 0);
+            waitpid(childPID, NULL, 0);
 
-            image_classifier(filePath,  topK);
 
+            readTemp(dataLine);
 
             free(filePath); 
-            free(mntPath);
             free(topKstring);
 
             len = 0;
@@ -87,26 +92,67 @@ int main()
             dataLine[len - 1] = buffer;
     }
 
+
     free(dataLine);
-    puts("1");
-
-    printf("%s\n", dlerror());
-    dlclose(handle);
-
-
-    puts("2");
     fclose(request);
-    puts("3");
     return 0;
 }
 
+void sandbox() {
+  scmp_filter_ctx ctx;
+  ctx = seccomp_init(SCMP_ACT_ALLOW);
+  
+  if (ctx == NULL) {
+    printf("seccomp error\n");
+    exit(0);
+  }
+
+  seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(rt_sigreturn), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(exit), 0);
+  seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(exit_group), 0);
+//   seccomp_rule_add(ctx, SCMP_ACT_KILL, SCMP_SYS(openat), 0);
+//   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(open), 1, SCMP_A0(SCMP_CMP_EQ, open("imagenet.shortnames.list", O_RDONLY)));
+//   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(open), 1, SCMP_A0(SCMP_CMP_EQ, open("darknet.weights", O_RDONLY)));
+//   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(open), 1, SCMP_A0(SCMP_CMP_EQ, open("darknet.cfg", O_RDONLY)));
+//   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(open), 1, SCMP_A0(SCMP_CMP_EQ, open(filePath, O_RDONLY)));
+//   seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(open), 1, SCMP_A0(SCMP_CMP_EQ, open("temp.txt", O_WRONLY)));
+  
+  seccomp_load(ctx);
+}
+
+void makeResult(){
+    FILE *result = fopen("results.txt", "w");
+    fprintf(result, "");
+    fclose(result);
+}
+
+void readTemp(const char *dataLine){
+    FILE *temp = fopen("temp.txt", "r");
+    FILE *result = fopen("results.txt", "a");
+
+    fprintf(result, "[%s]\n", dataLine);
+    int ch; 
+    while((ch = fgetc(temp)) != EOF)
+        fputc(ch, result);
+
+    fclose(temp);
+    fclose(result);
+    remove("temp.txt");
+}
+
 int child(void *arg){
-    // mount(mntPath, "/", "", MS_BIND, 0);
-    // FILE *tmp = freopen("temp.txt", "w", stdout);
+    if(geteuid() == 0 && getegid() == 0){
+        seteuid(65534);
+        setegid(65534);
+    }
 
-    printf("%s %s %d\n", mntPath, filePath, topK);
-    // image_classifier(filePath, topK);
+    prctl(PR_SET_NO_NEW_PRIVS, 1);
 
-    // fclose(tmp);
+    FILE *tmp = freopen("temp.txt", "w", stdout);
+
+    sandbox();
+    image_classifier(filePath, topK);
+
+    fclose(tmp);
     return 0;
 }
